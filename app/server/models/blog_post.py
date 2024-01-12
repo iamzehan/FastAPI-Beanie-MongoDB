e762b2e760d224d8bdc3c12892fd0858c61432c8
Zehan <zehan.kareem@gmail.com>
2024-01-12 23:34:53 +0600

    change Model

--- app/server/models/blog_post.py
+++ app/server/models/blog_post.py
@@ -1,42 +1,42 @@
 from beanie import Document
 from datetime import datetime
-
+from typing import Optional
+from bson import ObjectId
 class Article(Document):
     title: str
     content: str
-    date: datetime = datetime.now()
+    date: Optional[datetime] = datetime.now()
     author: str
     
     class Settings:
         name = "articles_collection"
     
-    class Config:
-        schema_extra = {
-            "example": {
+    model_config= {
+        "json_schema_extra":{
+            "examples": [{
                 "title" : "Learn MongoDB and FastAPI",
-                "content": "Today we are going to learn \
-                            to use a Beanie Powered API with \
-                            MongoDB and FastAPI",
-                "date" : "1/9/2024 at 8:56 PM",
+                "content": "Today we are going to learn to use a Beanie Powered API with MongoDB and FastAPI",
                 "author" : "iamzehan"
             }
+            ]
         } 
+    }
         
 class Comments(Document):
     article_id: str
     content: str
-    date: datetime = datetime.now()
+    date: Optional[datetime] = datetime.now()
     owner: str
     
     class Settings:
-        collection_name = "comments_collection"
+        name = "comments_collection"
     
-    class Config:
-        example = {
-            "example_article": {
-                "article_id": "2034808asdkj3",
+    model_config = {
+        "json_schema_extra":{
+            "examples": [{
+                "article_id" : "65a15a201b77ea7ebddb90c3",
                 "content": "Why do we use Beanie?",
-                "date" : "1/9/2024 at 9:32 PM",
                 "owner" : "ziaul"
-            }
+            }]
         } 
+    }
