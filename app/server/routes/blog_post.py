f350a6d84d0e7944c0e9a2d2a4cacceeacb61c85
Zehan <zehan.kareem@gmail.com>
2024-01-12 23:39:22 +0600

    updated responses

--- app/server/routes/blog_post.py
+++ app/server/routes/blog_post.py
@@ -1,36 +1,55 @@
-from typing import List
 from urllib.parse import unquote
-from beanie import PydanticObjectId
 from fastapi import APIRouter, HTTPException
-from fastapi.encoders import jsonable_encoder
 from fastapi.responses import JSONResponse
+
 from server.models.blog_post import Article, Comments
-from bson import ObjectId
+
 router = APIRouter()
 
 @router.post("/create", response_description="Blog added to the Database.")
-async def add_blog_post(blog: Article) -> dict:
+async def add_blog_post(blog: Article) -> JSONResponse:
+    """
+    Endpoint to add a new blog post to the database.
+    """
     post = await blog.create()
-    return {"message": "Blog posted successfully!",
-            "blog": post}
+    return JSONResponse(
+        content={"message": "Blog posted successfully!", "blog": post},
+        status_code=201,
+    )
 
 @router.post("/all/author={author}", response_description="Posts by an author")
-async def get_blogs_by_author(author: str) -> List[Article]:
-    posts = await Article.find({"author": unquote(author)},sort=[("date", -1)]).to_list()
+async def get_blogs_by_author(author: str) -> JSONResponse:
+    """
+    Endpoint to retrieve blog posts by a specific author.
+    """
+    posts = await Article.find({"author": unquote(author)}, sort=[("date", -1)]).to_list()
     
     if not posts:
         raise HTTPException(status_code=404, detail=f"No posts found for the author {author}")
-    
-    return posts
 
-@router.post("/comments/create/")
-async def add_a_comment(comment: Comments) -> dict:
+    return JSONResponse(
+        content={"message": f"Posts by author {author}", "posts": posts},
+        status_code=200,
+    )
+
+@router.post("/comments/create/", response_description="Comment added successfully.")
+async def add_a_comment(comment: Comments) -> JSONResponse:
+    """
+    Endpoint to add a comment to a blog post.
+    """
     comment = await comment.create()
-
-    return {"message": "Comment posted successfully!",
-            "comment": comment}
+    return JSONResponse(
+        content={"message": "Comment posted successfully!", "comment": comment},
+        status_code=201,
+    )
     
-@router.get("/comments/read/article_id={article_id}")
-async def read_all_comments(article_id: str) -> List[Comments]:
-    comments = await Comments.find({"article_id": unquote(article_id)},sort=[("date", -1)]).to_list()
-    return comments
+@router.get("/comments/read/article_id={article_id}", response_description="Retrieve comments for a blog post.")
+async def read_all_comments(article_id: str) -> JSONResponse:
+    """
+    Endpoint to retrieve all comments for a specific blog post.
+    """
+    comments = await Comments.find({"article_id": unquote(article_id)}, sort=[("date", -1)]).to_list()
+    return JSONResponse(
+        content={"message": f"Comments for article {article_id}", "comments": comments},
+        status_code=200,
+    )
