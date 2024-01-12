1af00f5f03e27d9462b3ce0afaed6182a7b3d2ed
Zehan <zehan.kareem@gmail.com>
2024-01-12 23:30:46 +0600

    added route /blogs

--- app/server/app.py
+++ app/server/app.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI
 from server.database import init_db
-from server.routes.product_review import router as Router
+from server.routes.product_review import router as ReviewRouter
+from server.routes.blog_post import router as BlogRouter
 
 
 app = FastAPI(docs_url="/")
-app.include_router(Router, tags=["Product Reviews"], prefix="/reviews")
-
+app.include_router(ReviewRouter, tags=["Product Reviews"], prefix="/reviews")
+app.include_router(BlogRouter, tags=["Blog Posts"], prefix="/blogs")
 
 @app.on_event("startup")
 async def start_db():
