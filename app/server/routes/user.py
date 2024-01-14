fb4ca93fe85206950e20f209fff447c3d6d2dd98
Zehan <zehan.kareem@gmail.com>
2024-01-14 23:40:35 +0600

    rm -unwanted imports

--- app/server/routes/user.py
+++ app/server/routes/user.py
@@ -1,32 +1,22 @@
 from typing import Annotated
-from datetime import timedelta
 
 from fastapi import Depends, HTTPException, status, APIRouter
-from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from fastapi.security import OAuth2PasswordRequestForm
 
-from app.auth.user import User
-from app.auth.token_model import Token
-from app.auth.auth_handler import authenticate_user, get_current_active_user
-
-
-
-from app.auth.auth_handler import (
-    get_hashed_password,
-    verify_password,
-    create_access_token,
-    create_refresh_token
+from auth.user import User, UserLogin
+from auth.token_model import Token
+from auth.auth_handler import(
+    authenticate_user,
+    get_current_active_user ,
+    create_access_token
 )
+
 
 router = APIRouter()
 
-
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
-
-
-
 @router.post("/token")
 async def login_for_access_token(
-    form_data: Annotated[OAuth2PasswordRequestForm, Depends()]
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends(UserLogin)]
 ) -> Token:
     user = authenticate_user(User, form_data.username, form_data.password)
     if not user:
@@ -35,10 +25,7 @@ async def login_for_access_token(
             detail="Incorrect username or password",
             headers={"WWW-Authenticate": "Bearer"},
         )
-    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-    access_token = create_access_token(
-        data={"sub": user.username}, expires_delta=access_token_expires
-    )
+    access_token = create_access_token(subject=user.username)
     return Token(access_token=access_token, token_type="bearer")
 
 
