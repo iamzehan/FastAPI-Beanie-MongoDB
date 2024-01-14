ad4c04d6196b634ad1b2a53aeaaaa968057ef73c
Zehan <zehan.kareem@gmail.com>
2024-01-14 23:12:27 +0600

    auth routes added

--- app/server/app.py
+++ app/server/app.py
@@ -2,9 +2,11 @@ from fastapi import FastAPI
 from server.database import init_db
 from server.routes.product_review import router as ReviewRouter
 from server.routes.blog_post import router as BlogRouter
+from app.server.routes.user import router as UserRouter
 
 
 app = FastAPI(docs_url="/")
+app.include_router(UserRouter, tags=["Authentication"], prefix="/user")
 app.include_router(ReviewRouter, tags=["Product Reviews"], prefix="/reviews")
 app.include_router(BlogRouter, tags=["Blog Posts"], prefix="/blogs")
 
