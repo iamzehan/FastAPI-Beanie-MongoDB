3eab0fbc006e5f7002c27c2833cd4ce95f6141e2
Zehan <zehan.kareem@gmail.com>
2024-01-15 19:02:43 +0600

    Login Handled

--- app/auth/auth_handler.py
+++ app/auth/auth_handler.py
@@ -13,7 +13,7 @@ from passlib.context import CryptContext
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 
-
+# load the .env file
 load_dotenv()
 
 # let's determine the defaults
@@ -27,30 +27,36 @@ JWT_REFRESH_SECRET_KEY = os.environ['REFRESH_SECRET_KEY']
 
 # this is an instance to your password_context, that can encrypt and decrypt your password, verify them etc.
 password_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/user/login/token")
 
+# ---- HASHING PASSWORDS ---- 
 def get_hashed_password(password: str) -> str:
     # this function hashes your password and returns it
     return password_context.hash(password)
 
-
+# ---- VERIFY HASHED PASSWORD
 def verify_password(password: str, hashed_pass: str) -> bool:
     # this function varifies your password
     return password_context.verify(password, hashed_pass)
 
-def get_user(db, username: str):
-    if db.find({"username": username}).exists():
-        user_dict = db.get(username)
-        return UserInDB(**user_dict)
-    
-def authenticate_user(db, username: str, password: str):
-    user = db.find({"username" : username})
-    if not user:
-        return False
-    if not verify_password(password, user.hashed_password):
-        return False
+# ---- GET USER FROM DATABASE ----
+async def get_user(db: User, username: str):
+    user = await db.find_one(db.username==username)
     return user
 
+# ---- AUTHENTICATION w/ USERNAME, PASSWORD ----
+async def authenticate_user(db: UserInDB, username: str, password: str):
+    # we find the username in the database
+    user = await db.find_one(db.username ==  username)
+    if not user:
+        return False
+    # if the user is found, we then verify the password, with the stored hashed password
+    if not verify_password(password, user.password):
+        return False
+    # after verification we return the user entity
+    return user
+
+# ---- GET CURRENT USER ----
 async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
@@ -58,18 +64,24 @@ async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
         headers={"WWW-Authenticate": "Bearer"},
     )
     try:
+        # this right here decodes the jwt token 
         payload = jwt.decode(token, JWT_SECRET_KEY, algorithms=[ALGORITHM])
+        # then stores the subject from payload as username
         username: str = payload.get("sub")
         if username is None:
             raise credentials_exception
+        # then it sets the token data according to the username
         token_data = TokenData(username=username)
     except JWTError:
         raise credentials_exception
-    user = get_user(User, username=token_data.username)
+    # then it gets the user from the database by separating username from the token data
+    user = await get_user(db=User, username=token_data.username)
+    print("-----------------", user)
     if user is None:
         raise credentials_exception
     return user
 
+# this function gets the current active user
 async def get_current_active_user(current_user: User = Depends(get_current_user)):
     if current_user.disabled:
         raise HTTPException(status_code=400, detail="Inactive user")
