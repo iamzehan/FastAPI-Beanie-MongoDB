faad234ad56cc049f65a43185d95f74d3eeb76f6
Zehan <zehan.kareem@gmail.com>
2024-01-16 20:45:16 +0600

    connect to mongodb cloud database

--- app/server/database.py
+++ app/server/database.py
@@ -4,10 +4,16 @@ from auth.auth_user import User
 from server.models.blog_post import Article, Comments
 from server.models.product_review import ProductReview
 
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
 async def init_db():
-    client = motor.motor_asyncio.AsyncIOMotorClient(
-        "mongodb://localhost:27017/"
-    )
+    # we are accessing our connection string via environment variables
+    # because it's not very wise to just keep it out in the open
+    client = motor.motor_asyncio.AsyncIOMotorClient(os.environ['MONGODB_CONNECTION_STRING'])
 
     await init_beanie(database=client.blog_review, document_models=[ProductReview, Article, Comments, User])
     
