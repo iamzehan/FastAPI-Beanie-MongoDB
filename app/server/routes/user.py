19e3e1f3aaa1c54a0f263be4d78dad9679e5ae54
Zehan <zehan.kareem@gmail.com>
2024-01-16 17:45:02 +0600

    add refresh token to login

--- app/server/routes/user.py
+++ app/server/routes/user.py
@@ -1,14 +1,13 @@
 from typing import Annotated
 
-from fastapi import Depends, HTTPException, status, APIRouter, Form
-from fastapi.security import OAuth2PasswordRequestForm
-from uuid import uuid4
-from app.auth.auth_user import User, UserLogin, UserSignUp, UserInDB
-from app.auth.auth_token import Token
+from fastapi import Depends, HTTPException, status, APIRouter
+from auth.auth_user import User, UserLogin, UserSignUp, UserInDB
+from auth.auth_token import Token
 from auth.auth_handler import(
     authenticate_user,
     get_current_active_user ,
     create_access_token,
+    create_refresh_token,
     get_hashed_password
 )
 
@@ -21,9 +20,9 @@ async def create_user(form_data: UserSignUp = Depends()):
     await userdb.create()
     return userdb
 
-@router.post("/login/token", summary="User Login", response_model=Token)
+@router.post("/login", summary="User Login", response_model=Token)
 async def login_for_access_token(
-    form_data: Annotated[OAuth2PasswordRequestForm, Depends()]
+    form_data=Depends(UserLogin)
 ) -> Token:
     # this function is going to authenticate the user and returns the user entity
     user = await authenticate_user(db = UserInDB, username=form_data.username, password=form_data.password)
@@ -36,8 +35,9 @@ async def login_for_access_token(
         )
     # upon findng the user entity it will create an access token, that lasts about 30 minutes.
     access_token = create_access_token(subject=user.username)
+    refresh_token = create_refresh_token(subject=user.username)
     # then it will return the access token
-    return Token(access_token=access_token, token_type="bearer")
+    return Token(access_token=access_token, refresh_token=refresh_token, token_type="bearer")
 
 
 @router.get("/me/", response_model=User)
